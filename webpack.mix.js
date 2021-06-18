84e9b0352dcfdb75bfadc9dd55c75fbfb0827c23
Vituz <vtbattaglia@gmail.com>
2021-06-18 10:21:40 +0200

    Add buttons icon

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,7 @@ mix.
 js('src/app.js', 'js')
 .sass('src/app.scss', 'css')
 .setPublicPath('dist');
+
+mix.options({
+    processCssUrls: false // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+  });
